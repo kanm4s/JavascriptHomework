1ceff1f26cfb9205ae8fe601b80bd258a28dc8b8
kanm4s <kanm4s@gmail.com>
2022-03-28 22:31:07 +0700

    fix Js05 03LoopThrough 04

--- JS05-Object/03-Loop-through-Object/04/main.js
+++ JS05-Object/03-Loop-through-Object/04/main.js
@@ -1,20 +1,40 @@
-function createObjFromProperties(obj) {
-    let properties = {};
-    for (let key in obj) {
-        if (key !== "stop") {
-            properties[key] = obj[key];
-        }
-    }
-    return properties;
+// function createObjFromProperties(obj) {
+//     let properties = {};
+//     for (let key in obj) {
+//         if (key !== "stop") {
+//             properties[key] = obj[key];
+//         }
+//     }
+//     return properties;
+// }
+
+// let person = {
+//     firstName: "Kan",
+//     surName: "Parker",
+//     age: 30,
+//     stop: "stop",
+// };
+
+// let newObj = createObjFromProperties(person);
+
+// console.log(newObj);
+
+function createObj() {
+  let keyInput = prompt("Enter key");
+  let valueInput = prompt("Enter value");
+  let obj = {};
+  
+  while (keyInput && valueInput) {
+      if (keyInput === "stop" || valueInput === "stop") {
+          return obj;
+      } else {
+          obj[keyInput] = valueInput;
+          keyInput = prompt("Enter key");
+          valueInput = prompt("Enter value");
+      }
+  }
+  return obj;
 }
 
-let person = {
-    firstName: "Kan",
-    surName: "Parker",
-    age: 30,
-    stop: "stop",
-};
-
-let newObj = createObjFromProperties(person);
-
-console.log(newObj);
+let testobj = createObj()
+console.log(testobj)
